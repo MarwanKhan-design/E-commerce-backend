973f845b52002404eb7173ed6fbf65796a43499b
Marwan Khan <marwanpsh@gmail.com>
2021-06-09 15:38:37 +0500

    Signin and Signout success

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { signup } from '../controller/user.js'
+import { signup, signin, signout } from '../controller/user.js'
 import { userSignUpValidator } from '../validator/index.js'
 
 const router = express.Router()
 
 router.post('/signup', userSignUpValidator, signup)
+router.post('/signin', signin)
+router.get('/signout', signout)
 
 export default router
